675f2755ab8359aabd536c1cc2fee68f83cd2854
Frederico Braga <frederico.braga@lyfegen.com>
2023-01-06 17:05:09 +0100

    button props and slot

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	export let color = 'white';
-	export let text: string;
+	export let color: string = 'white';
+	export let text: string = '';
 </script>
 
 <button on:click class="hover:border-b-{color} hover:border-b-2 -pb-4 hover:-mb-0.5 pb-2">
-	<span class="hover:bg-stone-100 rounded-lg p-2.5 "> {text} </span>
+	<span class="hover:bg-stone-100 rounded-lg p-2.5 "> {text} <slot /> </span>
 </button>
