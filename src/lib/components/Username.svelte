06115fe8f5cb77521fe02e9da5b037c8ee2e219a
Frederico Braga <frederico.braga@lyfegen.com>
2023-01-06 16:26:22 +0100

    username styles

--- src/lib/components/Username.svelte
+++ src/lib/components/Username.svelte
@@ -10,35 +10,35 @@
 	let showTooltip = false;
 </script>
 
-<p class="font-normal from-neutral text-sm">USERNAME</p>
-<span class="flex items-center h-6">
-	<p
-		class="truncate font-mono"
-		on:mouseover={() => (showTooltip = true)}
-		on:focus={() => (showTooltip = true)}
-		on:mouseleave={() => (showTooltip = false)}
-		use:usernameRef
-	>
-		{#if showTooltip}
-			<div class="bg-black p-2 rounded-md text-white" id="tooltip" use:popperUsernameContent>
-				{username}
-			</div>
-		{/if}
-		{#if edit}
-			<input
-				value={username}
-				on:input
-				class="p-1 rounded-md border-2 mt-2 focus:outline-1 focus:text-gray-900"
-			/>
-		{:else}
+<span
+	on:mouseover={() => (showTooltip = true)}
+	on:focus={() => (showTooltip = true)}
+	on:mouseleave={() => (showTooltip = false)}
+>
+	{#if showTooltip}
+		<div class="bg-black p-2 rounded-md text-white" id="tooltip" use:popperUsernameContent>
 			{username}
-		{/if}
-	</p>
-	<CopyIcon
-		on:keypress={() => alert('copy')}
-		on:click={() => {
-			navigator.clipboard.writeText(username || '');
-			alert(`copied ${username}`);
-		}}
-	/>
+		</div>
+	{/if}
+	<p class="font-normal from-neutral text-sm">USERNAME</p>
+	<span class="flex items-center h-6 ">
+		<p class="truncate" use:usernameRef>
+			{#if edit}
+				<input
+					value={username}
+					on:input
+					class="p-1 rounded-md border-2 mt-2 focus:outline-1 focus:text-gray-900"
+				/>
+			{:else}
+				{username}
+			{/if}
+		</p>
+		<CopyIcon
+			on:keypress={() => alert('copy')}
+			on:click={() => {
+				navigator.clipboard.writeText(username || '');
+				alert(`copied ${username}`);
+			}}
+		/>
+	</span>
 </span>
